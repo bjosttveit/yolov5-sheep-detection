bf28bf127ca613c6ce8ab839368c543c18dbd35a
bjosttveit <bjornost@stud.ntnu.no>
2021-10-12 12:54:14 +0200

    fix scripts

--- data.sh
+++ data.sh
@@ -4,4 +4,7 @@ kaggle datasets download bjosttveit/sheep-uav-yolo
 rm -rf datasets/ir datasets/rgb
 unzip -o sheep-uav-yolo.zip -d datasets
 rm sheep-uav-yolo.zip
+
+module purge
+module load Python/3.8.6-GCCcore-10.2.0
 python3 ./train_test_split.py
