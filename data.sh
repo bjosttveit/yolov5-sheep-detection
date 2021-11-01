379534c5788dc43e60d7dd073765cf42d1d2a617
Bjørnar Østtveit <bjosttveit@gmail.com>
2021-11-01 14:03:46 +0100

    job

--- data.sh
+++ data.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 
-kaggle datasets download bjosttveit/sheep-uav-yolo
+module purge
+module load Python/3.8.6-GCCcore-10.2.0
+
+kaggle datasets download -d bjosttveit/sheep-uav-yolo
 rm -rf datasets/ir datasets/rgb
 unzip -o sheep-uav-yolo.zip -d datasets
 rm sheep-uav-yolo.zip
 
-module purge
-module load Python/3.8.6-GCCcore-10.2.0
 python3 ./train_test_split.py
