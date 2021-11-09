6e9f45c15b779470b9579e117e2c3006a3f16ebd
bjosttveit <bjornost@stud.ntnu.no>
2021-11-09 16:40:36 +0100

    jadda

--- get_models.py
+++ get_models.py
@@ -5,19 +5,17 @@ api = wandb.Api()
 
 runs = [
     {"artifact": "run_3kpe4dbj_model:v0", "model": "RGB-n"},
-    {"run": "233f17y6", "model": "RGB-s"},
-    {"run": "3p0v997m", "model": "RGB-n6"},
-    {"run": "1lj0oubv", "model": "RGB-m"},
-    {"run": "130r3xgx", "model": "RGB-s6"},
-    {"run": "3r3v0n9c", "model": "RGB-l"},
-    {"run": "39plnshp", "model": "RGB-x"},
+    {"artifact": "run_233f17y6_model:v0", "model": "RGB-s"},
+    {"artifact": "run_3p0v997m_model:v0", "model": "RGB-n6"},
+    {"artifact": "run_1lj0oubv_model:v0", "model": "RGB-m"},
+    {"artifact": "run_130r3xgx_model:v0", "model": "RGB-s6"},
+    {"artifact": "run_3r3v0n9c_model:v0", "model": "RGB-l"},
+    {"artifact": "run_39plnshp_model:v0", "model": "RGB-x"},
 ]
 
 if not os.path.exists('models'):
     os.makedirs('models')
 
-api.artifact(f"YOLOv5/{runs[0]['artifact']}").download(root="./models")
-
-#for run in runs:
-#    api.run(f"bjosttveit/YOLOv5/{run['run']}").file("best.pt").download(root="./models", replace=True)
-#    break
+for run in runs:
+    api.artifact(f"YOLOv5/{run['artifact']}").download(root="./models")
+    os.rename("models/best.pt", f"models/{run['model']}.pt")
