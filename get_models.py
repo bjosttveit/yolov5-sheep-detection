eb6267162effdc21d61452c72a508fd3aec8d17a
bjosttveit <bjornost@stud.ntnu.no>
2021-11-09 16:37:21 +0100

    pls

--- get_models.py
+++ get_models.py
@@ -4,7 +4,7 @@ import wandb
 api = wandb.Api()
 
 runs = [
-    {"run": "3kpe4dbj", "model": "RGB-n"},
+    {"artifact": "run_3kpe4dbj_model:v0", "model": "RGB-n"},
     {"run": "233f17y6", "model": "RGB-s"},
     {"run": "3p0v997m", "model": "RGB-n6"},
     {"run": "1lj0oubv", "model": "RGB-m"},
@@ -16,9 +16,7 @@ runs = [
 if not os.path.exists('models'):
     os.makedirs('models')
 
-files = api.run(f"bjosttveit/YOLOv5/{runs[0]['run']}").files()
-for f in files:
-    print(f)
+api.artifact(f"YOLOv5/{runs[0]['artifact']}").download(root="./models")
 
 #for run in runs:
 #    api.run(f"bjosttveit/YOLOv5/{run['run']}").file("best.pt").download(root="./models", replace=True)
