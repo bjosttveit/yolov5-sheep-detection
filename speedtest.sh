65b1b41d32e8b0fda8aec02505c5d6bc194a41db
Bjørnar Østtveit <bjosttveit@gmail.com>
2021-11-15 20:05:04 +0100

    more

--- speedtest.sh
+++ speedtest.sh
@@ -1,2 +1,10 @@
 #!/bin/sh
 
+# Uncomment when running on IDUN
+#module purge
+#module load Python/3.8.6-GCCcore-10.2.0
+
+mkdir -p speeds
+
+cd yolov5
+python3 val.py --data ../datasets/rgb.yaml --weights ../models/RGB-${1}.pt --project ../tests --name RGB-${1} --exist-ok --task=speed --batch-size=1 2>&1 | sed -En 's/.*pre-process, (.*)ms inference.*/\1/p' >> ../speeds/RGB-${1}.txt
--- val.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-# Uncomment when running on IDUN
-#module purge
-#module load Python/3.8.6-GCCcore-10.2.0
-
-cd yolov5
-python3 val.py --data ../datasets/rgb.yaml --weights ../models/RGB-n.pt --task speed
