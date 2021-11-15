c6c991e6c6baf5dd1a7e3f0bb3de75ca0e1221b2
Bjørnar Østtveit <bjosttveit@gmail.com>
2021-11-15 12:02:36 +0100

    test set

--- train_test_split.py
+++ train_test_split.py
@@ -3,5 +3,5 @@ sys.path.insert(0, './yolov5')
 
 from utils.datasets import *
 
-autosplit('datasets/ir/images', (0.8, 0.2, 0.0))
-autosplit('datasets/rgb/images', (0.8, 0.2, 0.0))
+autosplit('datasets/ir/images', (0.7, 0.15, 0.15))
+autosplit('datasets/rgb/images', (0.7, 0.15, 0.15))
