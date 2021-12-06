df17ce7605624dab30940a3240662af0ad6ff795
Bjørnar Østtveit <bjosttveit@gmail.com>
2021-12-06 12:16:30 +0100

    detect

--- detect6.sh
+++ detect6.sh
@@ -5,4 +5,4 @@
 #module load Python/3.8.6-GCCcore-10.2.0
 
 cd yolov5
-python3 detect.py --source ../datasets/rgb/images --weights ../models/RGB-n.pt --nosave --img 1280
+python3 detect.py --source ../datasets/rgb/images --weights ../models/RGB-${1}.pt --nosave --img 1280
