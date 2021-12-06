1c566946ecee451a156e457f056f6841ab5363b5
= <bjosttveit@gmail.com>
2021-12-06 11:46:08 +0100

    allspeedtests

---
+++ all-speedtests.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+#./speedtest.sh n $1
+./speedtest.sh s $1
+./speedtest6.sh n6 $1
+./speedtest6.sh s6 $1
+./speedtest.sh m $1
+./speedtest6.sh m6 $1
+./speedtest.sh l $1
+./speedtest6.sh l6 $1
+./speedtest.sh x $1
+./speedtest6.sh x6 $1
